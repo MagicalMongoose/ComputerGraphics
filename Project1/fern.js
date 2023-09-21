81705602b8d09928c2ab3ad0dd74758a03d2edc6
Drew <drew1681@gmail.com>
2023-09-21 14:49:54 -0500

    this became a useless jumbled mess

--- Project1/fern.js
+++ Project1/fern.js
@@ -5,13 +5,10 @@ var points = [];
 //var depth = 6; // branches^depth = number of points in the fern
 var color = 1;      // choose color for display, press key 'c'
 var drawAlt = 1;  // choose patten for display, mouse click
-var debug = false;
-var x, y;
-var nextX, nextY;
-var total = 50000;
+var debug = true;
+var total = 50;
 var i = 0;
-var xMin, xMax = 0;
-var yMin, yMax = 0;
+
 
 function main()
 {
@@ -20,18 +17,14 @@ function main()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { console.log( "WebGL isn't available" ); return; }
 
-    x = 0;
-    y = 0;
-    points.push(vec2(x,y));
-
     //sets
     var presetValues1 = 
     [       //s0    s1    s2     s3
-        /*a*/[0.0,  0.2,   -0.15, 0.75],
-        /*b*/[0.0,  -0.26, 0.28,  0.04],
-        /*c*/[0.0,  0.23,  0.26,  -0.04],
+        /*a*/[0.0,  0.2,  -0.15,  0.75],
+        /*b*/[0.0, -0.26,  0.28,  0.04],
+        /*c*/[0.0,  0.23,  0.26, -0.04],
         /*d*/[0.16, 0.22,  0.24,  0.85],
-        /*e*/[0.01,  0.01,   0.01,   0.01],
+        /*e*/[0.01, 0.01,  0.01,  0.01],
         /*f*/[0.0,  1.6,   0.44,  1.6],
         /*p*/[0.1,  0.08,  0.08,  0.74] //p[] sum == 1
     ]
@@ -58,7 +51,9 @@ function main()
 
     //primary function
     //fern(presetValues, x, y, depth);
-    generatePoints(presetValues1);
+    points = generatePoints(presetValues1);
+    console.log("points: ", points);
+
     //  Configure WebGL
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
 
@@ -133,17 +128,68 @@ function determineSet(presetValues)
     return setNum;
 }
 
+function formula(x,y,setNum)
+{
+    console.log("formula starting with: ", x, y, setNum);
+    var nextX = 0;
+    var nextY = 0;
+    if (setNum == 0)
+    {
+        nextX = (0.0*x) + (0.0*y) + 0.0;
+        nextY = (0.0*x) + (0.16*y) + 0.0;
+    }
+    else if (setNum == 1)
+    {
+        nextX = (0.85*x) + (0.04*y) + 0.0;
+        nextY = (0.04*x) + (0.85*y) + 1.6;
+    }
+    else if (setNum == 2)
+    {
+        nextX = (0.2*x) + (0.26*y) + 0.0;
+        nextY = (0.23*x) + (0.22*y) + 1.6;
+    }
+    else if (setNum == 3)
+    {
+        nextX = (-0.15*x) + (0.28*y) + 0.0;
+        nextY = (0.26*x) + (0.24*y) + 0.44;
+    }
+    console.log("formula result: ", nextX, nextY);
+    return nextX, nextY;
+}
+
+//formula but more modular
+function calculatePoint(presetValues, x, y)
+{
+    var set = determineSet(presetValues);
+    var a = presetValues[0][set];
+    var b = presetValues[1][set];
+    var c = presetValues[2][set];
+    var d = presetValues[3][set];
+    var e = presetValues[4][set];
+    var f = presetValues[5][set];
+
+    //general form of the series: 
+    nextX = (a*x) + (b*y) + e;
+    nextY = (c*x) + (d*y) + f;
+
+    return nextX, nextY;
+}
+
 //generate a list of points that will be used to draw the fern
 function generatePoints(presetValues) 
 {
     points = []; //clear points array
-    xMin = 0;
-    xMax = 0;
-    yMin = 0;
-    yMax = 0;
+    let x = 0;
+    let y = 0;
+    var xMin, xMax = 0;
+    var yMin, yMax = 0;
+    let i = 0;
+    
 
     while (i < total)
     {
+        //let nextX, nextY;
+        /*
         var set = determineSet(presetValues);
         var a = presetValues[0][set];
         var b = presetValues[1][set];
@@ -154,45 +200,81 @@ function generatePoints(presetValues)
 
         //general form of the series: 
         nextX = (a*x) + (b*y) + e;
-        nextY = (c*x) + (d*y) + f/3;
+        nextY = (c*x) + (d*y) + f;
+        */
+        /*
+        console.log("looping with: ", x, y);
+        nextX, nextY = formula(x, y, determineSet(presetValues));
+        //nextX, nextY = calculatePoint(presetValues, x, y);
 
-        //get the x/yMax 
+        console.log("nextX: " + nextX + " nextY: " + nextY);
+        */
+        const r = Math.random();
+
+        let nextX, nextY;
+
+        if (r <= 0.01) {
+            nextX = 0;
+            nextY = 0.16 * y;
+        } else if (r <= 0.86) {
+            nextX = 0.85 * x + 0.04 * y;
+            nextY = -0.04 * x + 0.85 * y + 1.6;
+        } else if (r <= 0.93) {
+            nextX = 0.2 * x - 0.26 * y;
+            nextY = 0.23 * x + 0.22 * y + 1.6;
+        } else {
+            nextX = -0.15 * x + 0.28 * y;
+            nextY = 0.26 * x + 0.24 * y + 0.44;
+        }
+        //console.log("newX, newY: ", nextX, nextY);
+        //x = nextX;
+        //y = nextY;
+        //console.log("loop after formula: x, y: ", x, y);
+
+        //get the x/yMax and //get the x/yMin
+        
         if (nextX > xMax)
         {xMax = nextX;}
+        if (nextX < xMin && nextX != xMax)
+        {xMin = nextX;}
+
         if (nextY > yMax)
         {yMax = nextY;}
-
-        //get the x/yMin
-        if (nextX < xMin)
-        {xMin = nextX;}
-        if (nextY < yMin)
+        if (nextY < yMin && nextY != yMax)
         {yMin = nextY;}
         
         //normalization
-        nextX = (nextX - xMin) / (xMax - xMin);
-        nextY = (nextY - yMin) / (yMax - yMin);
+        if (xMax != xMin)
+        {nextX = (nextX - xMin) / (xMax - xMin);}
+        if (yMax != yMin)
+        {nextY = (nextY - yMin) / (yMax - yMin);}
+
+        console.log("normalized nextX: " + nextX +  nextX + " normalized nextY: " + nextY);
         
         //update x/y so the fern moves as intended
         x = nextX;
         y = nextY;
-        
-        points.push(vec2(nextX, nextY));
+
+        points.push(vec2(x, y));
 
         if (debug) 
         {
-            /*console.log("a: ", a);
+            /*
+            console.log("a: ", a);
             console.log("b: ", b);
             console.log("c: ", c);
             console.log("d: ", d);
             console.log("e: ", e);
-            console.log("f: ", f);*/
+            console.log("f: ", f);
+            */
             console.log("x y: ", x, y);
-            console.log("xMax yMax: ", xMax, yMax);
-            console.log("xMin yMin: ", xMin, yMin);
+            //console.log("xMax yMax: ", xMax, yMax);
+            //console.log("xMin yMin: ", xMin, yMin);
         }
         
         i++
     }
+    return points;
 }
     
 /*
@@ -220,8 +302,8 @@ function fern(presetValues, x, y, depth)
 function render() 
 {
     if (debug) 
-    {console.log(points);}
-    
+    {//console.log(points);}
+    }
     gl.clear(gl.COLOR_BUFFER_BIT);
 
     gl.uniform1i(gl.getUniformLocation(program, "colorIndex"), color);
